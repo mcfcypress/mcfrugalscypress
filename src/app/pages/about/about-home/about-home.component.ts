1768406ab824c5f9d7beb929d4f81b8d377d4013
Mika <mikagurrola90@gmail.com>
2018-07-08 13:10:40 -0500

    added option to to angular animations

--- src/app/pages/about/about-home/about-home.component.ts
+++ src/app/pages/about/about-home/about-home.component.ts
@@ -1,21 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import {trigger, stagger, animate, style, group, query as q, transition, keyframes} from '@angular/animations';
-const query = (s,a,o={optional:true})=>q(s,a,o);
+import {trigger, stagger, animate, style, group, query, transition, keyframes} from '@angular/animations';
 
 
 export const pageTransition = trigger('pageTransition', [
   transition(':enter', [
-    query('article', style({ opacity: 0 })),
+    query('article', style({ opacity: 0 }), {optional: true}),
     query('article', stagger(500, [
       style({ transform: 'translateY(100px)' }),
       animate('1s cubic-bezier(.75,-0.48,.26,1.52)', style({transform: 'translateY(0px)', opacity: 1})),
-    ])),
+    ]), {optional: true}),
   ]),
   transition(':leave', [
     query('article', stagger(50, [
       style({ transform: 'translateY(0px)', opacity: 1 }),
       animate('1s cubic-bezier(.75,-0.48,.26,1.52)', style({transform: 'translateY(100px)', opacity: 0})),
-    ])),        
+    ]), {optional: true}),        
   ])
 ]);
 
--- src/app/router.animations.ts
+++ src/app/router.animations.ts
@@ -1,27 +1,26 @@
-import {sequence, trigger, stagger, animate, style, group, query as q, transition, keyframes, animateChild} from '@angular/animations';
-const query = (s,a,o={optional:true})=>q(s,a,o);
+import {sequence, trigger, stagger, animate, style, group, query, transition, keyframes, animateChild} from '@angular/animations';
 
 
 export const routerTransition = trigger('routerTransition', [
   // when transitioning ever
   transition('* => *', [
-    query(':enter, :leave', style({ position: 'fixed', width:'100%' })),
-    query(':enter', style({ transform: 'translateX(100%)' })),
-    query(':leave', animateChild()), 
+    query(':enter, :leave', style({ position: 'fixed', width:'100%' }), {optional: true}),
+    query(':enter', style({ transform: 'translateX(100%)' }), {optional: true}),
+    query(':leave', animateChild(), {optional: true}), 
     sequence([
       group([
         query(':leave', [
           style({ transform: 'translateX(0%)' }),
           animate('500ms cubic-bezier(.75,-0.48,.26,1.52)', 
             style({ transform: 'translateX(-100%)' }))
-        ]),
+        ], {optional: true}),
         query(':enter', [
           style({ transform: 'translateX(100%)' }),
           animate('500ms cubic-bezier(.75,-0.48,.26,1.52)', 
             style({ transform: 'translateX(0%)' })),
-        ]),
+        ], {optional: true}),
       ]),
     ]),
-    query(':enter', animateChild()),
+    query(':enter', animateChild(), {optional: true}),
   ]),
 ]);
